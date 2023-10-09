306313dd6984a4c3c116d38a5dd14b8504af9e2b
Mario <NA>
2023-10-09 03:29:03 -0500

    Updated code for Homework

--- Program.cs
+++ Program.cs
@@ -6,16 +6,27 @@ class Program
     static void Main()
     {
 
-        int n = 8000000;
+        int n = 8000;
         double[,] numbers;
+        float[,] fnumbers;
+        int[,] wholenumbers;
 
         Stopwatch timer = new Stopwatch();
         numbers = GenRandomNumbers(n);
+        fnumbers = GenRandomFloatNumbers(n);
+        wholenumbers = GenRandomWholeNumbers(n);
+
 
         timer.Start();
         AddNumbers(numbers, n);
         timer.Stop();
-        Console.WriteLine("Additions");
+        Console.WriteLine("Addition with Doubles");
+        Console.WriteLine("Elapsed time = " + timer.ElapsedMilliseconds + "ms" + timer.ElapsedTicks + "Ticks" );
+
+        timer.Start();
+        AddFloatNumbers(fnumbers, n);
+        timer.Stop();
+        Console.WriteLine("Addition with Floats");
         Console.WriteLine("Elapsed time = " + timer.ElapsedMilliseconds + "ms" + timer.ElapsedTicks + "Ticks" );
 
         timer.Start();
@@ -24,32 +35,26 @@ class Program
         Console.WriteLine("Multiplications");
         Console.WriteLine("Elapsed time = " + timer.ElapsedMilliseconds + "ms" + timer.ElapsedTicks + "Ticks" );
 
-
-        timer.Start();
-        Sin(numbers,n);
-        timer.Stop();
-        Console.WriteLine("Sin");
-        Console.WriteLine("Elapsed time = " + timer.ElapsedMilliseconds + "ms" + timer.ElapsedTicks + "Ticks" );
-
-        timer.Start();
-        Cos(numbers,n);
-        timer.Stop();
-        Console.WriteLine("Cos");
-        Console.WriteLine("Elapsed time = " + timer.ElapsedMilliseconds + "ms" + timer.ElapsedTicks + "Ticks" );
-
         timer.Start();
         Expo(numbers,n);
         timer.Stop();
-        Console.WriteLine("Expo");
+        Console.WriteLine("Math.Pow Power Function");
+        Console.WriteLine("Elapsed time = " + timer.ElapsedMilliseconds + "ms" + timer.ElapsedTicks + "Ticks" );
+
+        timer.Start();
+        Sqrt(numbers,n);
+        timer.Stop();
+        Console.WriteLine("Math.Sqrt Power Function");
+        Console.WriteLine("Elapsed time = " + timer.ElapsedMilliseconds + "ms" + timer.ElapsedTicks + "Ticks" );
+
+        
+        timer.Start();
+        DirectMultiplicationExpo(numbers,n);
+        timer.Stop();
+        Console.WriteLine("Direct Multiplication Expo Function");
         Console.WriteLine("Elapsed time = " + timer.ElapsedMilliseconds + "ms" + timer.ElapsedTicks + "Ticks" );
 
 
-
-        //int i;
-        //for (i=0; i<n; ++i)
-        //{
-        //    Console.WriteLine(numbers[i,0] + " " + numbers[i,1] + numbers[i,2]);
-        //}
     }
 
     static double[,] GenRandomNumbers(int count)
@@ -65,7 +70,42 @@ class Program
         return num;
     }
 
+     static float[,] GenRandomFloatNumbers(int count)
+    {
+        Random rand = new Random();
+        float[,] num = new float[count, 3];
+        for(int i=0; i< count; ++i)
+        {
+            num[i,0]= (float)(100.0 *rand.NextDouble());
+            num[i,1]= (float)(100.0 *rand.NextDouble());
+        }
+
+        return num;
+    }
+     static int[,] GenRandomWholeNumbers(int count)
+    {
+        Random rand = new Random();
+        int[,] num = new int[count, 3];
+        for(int i=0; i< count; ++i)
+        {
+            num[i,0]= (int)(10.0 *rand.NextDouble());
+            num[i,1]= (int)(10.0 *rand.NextDouble());
+        }
+
+        return num;
+    }
+
+
     static void AddNumbers(double[,]nums, int count)
+    {
+        int i;
+        for (i=0; i < count; ++i)
+        {
+            nums[i,2]= nums[i,0] + nums[i,1];
+        }
+    }
+
+    static void AddFloatNumbers(float[,]nums, int count)
     {
         int i;
         for (i=0; i < count; ++i)
@@ -83,35 +123,41 @@ class Program
         }
     }
 
+      static void DirectMultiplicationExpo(double[,] nums, int count)
+    {
 
-     static void Sin(double[,] nums, int count)
+    for(int j=0; j< count; ++j)
+    {
+            double index = nums[j, 0];
+            //Console.WriteLine("First number " + nums[j,0]);
+    
+        for (int i = 0; i < count + 1; i++)
+        {
+            nums[j,0] = nums[j,0]*index;
+            //Console.WriteLine(nums[j,0]);
+        }
+        
+    }
+
+
+}
+
+       static void Expo(double[,] nums, int count)
     {
         int i;
         for(i=0; i <count; ++i)
         {
-            nums[i, 2] = Math.Sin(nums[i, 0]); 
+            nums[i, 2] = Math.Pow (nums[i, 1],nums[i, 0]);
         }
     }
-        static void Cos(double[,] nums, int count)
+
+       static void Sqrt(double[,] nums, int count)
     {
         int i;
         for(i=0; i <count; ++i)
         {
-            nums[i, 2] = Math.Cos(nums[i, 0]);
+            nums[i, 1] = Math.Sqrt (nums[i, 0]);
         }
     }
-
-      static void Expo(double[,] nums, int count)
-    {
-        int i;
-        for(i=0; i <count; ++i)
-        {
-            nums[i, 1] = Math.Pow(nums[i, 0]);
-        }
-    }
-
-
-
-
 
 }
